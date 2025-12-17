a2fd5a4a55f5f9e004c950725c6ae98853f04fce
Omni-KingZeno <200784099+Omni-KingZeno@users.noreply.github.com>
2025-12-17 14:14:05 -0500

    Fix Dispose error for ALMStatusBar - Added a wait loop to ensure the form handle is created before proceeding - Use Invoke to close the form on the UI thread - Use Invoke when updating t.Count - Added check in the polling loop to avoid accessing disposed objects - Ensure the polling loop completes before continuing
    TransferLivingDex => LivingTransferDex
    - Groups "Generate Transfer Dex" with "Generate Living Dex" & "Generate Living Egg Dex"
    - Add shortcut "Alt+T" for Transfer Dex generation
    - Add tracking count to Transfer Dex
    - Display ALMStatusBar for Transfer dex

--- AutoLegalityMod/Plugins/LivingTransferDex.cs
+++ AutoLegalityMod/Plugins/LivingTransferDex.cs
@@ -1,40 +1,50 @@
-﻿using System;
-using System.IO;
-using System.Windows.Forms;
+﻿using AutoModPlugins.GUI;
 using AutoModPlugins.Properties;
 using PKHeX.Core;
 using PKHeX.Core.AutoMod;
+using System;
 using System.Collections.Generic;
-using Microsoft.VisualBasic.Devices;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using AutoModPlugins.GUI;
+using System.Windows.Forms;
 
 namespace AutoModPlugins;
 
-public class LivingDex : AutoModPlugin
+public class TransferLivingDex : AutoModPlugin
 {
-    public override string Name => "Generate Living Dex";
+    public override string Name => "Transfer Living Dex";
+
     public override int Priority => 1;
 
     protected override void AddPluginControl(ToolStripDropDownItem modmenu)
     {
         var ctrl = new ToolStripMenuItem(Name)
         {
-            Image = WinFormsUtil.GetIconForTheme(Resources.livingdex, Application.IsDarkModeEnabled),
-            ShortcutKeys = Keys.Alt | Keys.E,
+            Image = Resources.livingdex,
+            ShortcutKeys = Keys.Alt | Keys.T,
         };
-        ctrl.Click += GenLivingDex;
-        ctrl.Name = "Menu_LivingDex";
+        ctrl.Click += GenTLivingDex;
+        ctrl.Name = "Menu_TransferDex";
         modmenu.DropDownItems.Add(ctrl);
     }
 
-    private async void GenLivingDex(object? sender, EventArgs e)
+    private async void GenTLivingDex(object? sender, EventArgs e)
     {
-        var prompt = WinFormsUtil.Prompt(MessageBoxButtons.YesNo, $"Generate a Living Dex?");
-        if (prompt != DialogResult.Yes)
+        if (_settings.TransferVersion == GameVersion.Any)
+        {
+            WinFormsUtil.Alert("Please set a valid Transfer Version in the settings.");
             return;
+        }
+
+        var prompt = WinFormsUtil.Prompt(MessageBoxButtons.YesNo, $"Generate a Transfer Dex for {_settings.TransferVersion}?");
+        if (prompt != DialogResult.Yes)
+        {
+            return;
+        }
+
         var sav = SaveFileEditor.SAV;
-        var t = new ALMStatusBar("Living Dex", sav.MaxSpeciesID)
+        var t = new ALMStatusBar("Living Transfer Dex", sav.MaxSpeciesID)
         {
             Count = ModLogic.TrackingCount
         };
@@ -50,7 +60,7 @@ public class LivingDex : AutoModPlugin
         // After showing the status bar, then start the polling loop
         var pollingTask = Task.Run(() => PollingLoop(t));
 
-        var dex = await Task.Run(() => sav.GenerateLivingDex(sav.Personal));
+        var pkms = await Task.Run(() => sav.GenerateTransferLivingDex().ToArray());
         List<PKM> extra = [];
 
         // Now we can safely close the status bar
@@ -65,25 +75,22 @@ public class LivingDex : AutoModPlugin
         // waiting for the task to finish
         await pollingTask;
 
-        int generated = IngestToBoxes(sav, dex, extra);
-        System.Diagnostics.Debug.WriteLine($"Generated Living Dex with {generated} entries.");
+        int generated = IngestToBoxes(sav, pkms, extra);
+        System.Diagnostics.Debug.WriteLine($"Generated Living Transfer Dex with {pkms.Length} entries.");
         SaveFileEditor.ReloadSlots();
         if (extra.Count == 0)
             return;
 
-        prompt = WinFormsUtil.Prompt(MessageBoxButtons.YesNo, "This Living Dex does not fit in all boxes. Save the extra to a folder?");
+        prompt = WinFormsUtil.Prompt(MessageBoxButtons.YesNo, "This Living Transfer Dex does not fit in all boxes. Save the extra to a folder?");
         if (prompt != DialogResult.Yes)
             return;
 
         using var ofd = new FolderBrowserDialog();
         if (ofd.ShowDialog() != DialogResult.OK)
             return;
-        Span<byte> PokeData = stackalloc byte[sav.SIZE_PARTY];
+
         foreach (var f in extra)
-        {
-            f.WriteDecryptedDataParty(PokeData);
-            File.WriteAllBytes($"{ofd.SelectedPath}/{f.FileName}", PokeData);
-        }
+            await File.WriteAllBytesAsync($"{ofd.SelectedPath}/{f.FileName}", f.DecryptedPartyData);
     }
 
     private static void PollingLoop(ALMStatusBar t)
@@ -113,32 +120,31 @@ public class LivingDex : AutoModPlugin
             System.Threading.Thread.Sleep(50);
         }
     }
-    private static int IngestToBoxes(SaveFile sav, IEnumerable<PKM> list, IList<PKM> extra, bool overwrite, int slot = 0)
+
+    private static int IngestToBoxes(SaveFile sav, Span<PKM> list, IList<PKM> extra, int slot = 0)
     {
         int generated = 0;
         foreach (var pk in list)
         {
             generated++;
-            if (TryAdd(sav, extra, pk, overwrite, ref slot))
+            if (TryAdd(sav, extra, pk, ref slot))
                 continue;
             do
             {
                 slot++;
             }
-            while (!TryAdd(sav, extra, pk,overwrite, ref slot));
+            while (!TryAdd(sav, extra, pk, ref slot));
         }
         return generated;
     }
 
-    private static bool TryAdd(SaveFile sav, IList<PKM> extra, PKM pk, bool overwrite, ref int slot)
+    private static bool TryAdd(SaveFile sav, IList<PKM> extra, PKM pk, ref int slot)
     {
         if (slot >= sav.SlotCount)
         {
             extra.Add(pk);
             return true;
         }
-        if (!overwrite && sav.NextOpenBoxSlot(slot - 1) != slot)
-            return false;
         if (!sav.IsBoxSlotOverwriteProtected(slot))
         {
             sav.SetBoxSlotAtIndex(pk, slot++);
